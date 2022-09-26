37139074e68dda382811acdbf36b1762a187a45e
JonathanRamos <jonathansramos@gmail.com>
2022-09-26 05:44:23 -0400

    commit de segunda antes da aula

--- Escola/src/Main.java
+++ Escola/src/Main.java
@@ -1,14 +1,15 @@
 import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
-        String nome = "Aluno a";
-        String CPF = "123.456.789-00";
-        Date data = new Date();
-        String matricula = "1234356";
-        
-        Aluno a = new Aluno(matricula, nome, CPF, data);
-        
-        System.out.println("Como os atributos ficaram: ");
-        System.out.println(a.toString());
-    }
+	public static void main(String[] args) {
+		String nome = "Aluno a";
+		String CPF = "123.456.789-00";
+		Date data = new Date();
+		String matricula = "1234356";
+
+		Aluno a = new Aluno(matricula, nome, CPF, data);
+
+		System.out.println("Como os atributos ficaram: ");
+		System.out.println(a.toString());
+	}
 }
